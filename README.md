10cd87111f2ad8baf6088c8df31b09a8b88ccbbe
Tomas Thelander <tomas.thelander0@gmail.com>
2012-05-19 14:12:43 +0200

    dragging support

--- README.md
+++ README.md
@@ -15,55 +15,55 @@ Setup
 
 Include graph.js and create a canvas with suitable dimensions
 
-	<canvas id="mathgraph" width="535" height="560" style="border: 1px solid;">
-		MathGraph requires Canvas. Your browser is outdated.
-	</canvas>
+    <canvas id="mathgraph" width="535" height="560" style="border: 1px solid;">
+        MathGraph requires Canvas. Your browser is outdated.
+    </canvas>
 
 Configure MathGraph
 
-	// The following code is from index.html
-	
-	// Create new object with the canvas id as parameter
-	var m = new MathGraph('mathgraph');
-	    
+    // The following code is from index.html
+    
+    // Create new object with the canvas id as parameter
+    var m = new MathGraph('mathgraph');
+        
     // The second parameter determines when the point on the curve
     // should be moved. Default is 'click'
     //m = new MathGraph('mathgraph', 'drag');
     //m = new MathGraph('mathgraph', 'mousemove');
     
-	// Define the functions
-	m.Functions = {
-		f1 : function(x) {
-			return x*x*x + 3*x*x - 9*x - 12;
-		},
-		f2 : function(x) {
-			return 4*x + 3;
-		}
-	};
-	
-	// The color of the plotted function. Default is black.
-	m.FColors = {
-		f1 : '#FF0000'
-	};
-	
-	
-	// Properties of the coordinate system
-	m.MaxY = 50;
-	m.MinY = -50;
-	m.MaxX = 10;
-	m.MinX = -10; // The default values of these four are -10/10
-	
-	m.YTick = 6;
-	m.XTick = 1; // The distance between the points on the axes (default: 1)
-	
-	// Draws MathGraph:
-	m.Draw();
-	
-	// Draws a dot at a specific X-value.
-	// param1: function name. param2: X-value.
-	// param3: If all other dots should be removed before drawing.
-	m.DrawDot('f1',1,true);
-	m.DrawDot('f1',3,false);
+    // Define the functions
+    m.Functions = {
+        f1 : function(x) {
+            return x*x*x + 3*x*x - 9*x - 12;
+        },
+        f2 : function(x) {
+            return 4*x + 3;
+        }
+    };
+    
+    // The color of the plotted function. Default is black.
+    m.FColors = {
+        f1 : '#FF0000'
+    };
+    
+    
+    // Properties of the coordinate system
+    m.MaxY = 50;
+    m.MinY = -50;
+    m.MaxX = 10;
+    m.MinX = -10; // The default values of these four are -10/10
+    
+    m.YTick = 6;
+    m.XTick = 1; // The distance between the points on the axes (default: 1)
+    
+    // Draws MathGraph:
+    m.Draw();
+    
+    // Draws a dot at a specific X-value.
+    // param1: function name. param2: X-value.
+    // param3: If all other dots should be removed before drawing.
+    m.DrawDot('f1',1,true);
+    m.DrawDot('f1',3,false);
  
 
 
--- graph.js
+++ graph.js
@@ -1,8 +1,8 @@
 /*
-	MathGraph by Tomas Thelander
-	
-	This is an expanded and reworked version of the example from:
-	http://matt.might.net/articles/rendering-mathematical-functions-in-javascript-with-canvas-html/
+    MathGraph by Tomas Thelander
+    
+    This is an expanded and reworked version of the example from:
+    http://matt.might.net/articles/rendering-mathematical-functions-in-javascript-with-canvas-html/
 */
 
 
@@ -39,25 +39,25 @@ function MathGraph(id, mouse)
 {
     var dragging = false;
     
-	if (mouse == null)
-	{
-		mouse = 'click';
-	}
+    if (mouse == null)
+    {
+        mouse = 'click';
+    }
     else if (mouse == 'drag')
     {
         dragging = true;
         mouse = 'click';
     }
     
-	this.Canvas = document.getElementById(id);
-	this.Ctx = null;
+    this.Canvas = document.getElementById(id);
+    this.Ctx = null;
     
-	this.Width = this.Canvas.width;
-	this.Height = this.Canvas.height;
-	
-	this.MaxY = this.MaxX * this.Height / this.Width;
-	this.MinY = this.MinX * this.Height / this.Width;
-	
+    this.Width = this.Canvas.width;
+    this.Height = this.Canvas.height;
+    
+    this.MaxY = this.MaxX * this.Height / this.Width;
+    this.MinY = this.MinX * this.Height / this.Width;
+    
     
     this._addEvent(this.Canvas,mouse,function(e){
         if (e == null)
@@ -140,248 +140,248 @@ function MathGraph(id, mouse)
     }
     
     
-	this._addEvent(document,'keydown',function(e){
-		if (e == null)
-		{
-			e = window.event;
-		}
-		if (e.keyCode == 27)
-		{
-			this.Draw();
-		}
-	},this);
+    this._addEvent(document,'keydown',function(e){
+        if (e == null)
+        {
+            e = window.event;
+        }
+        if (e.keyCode == 27)
+        {
+            this.Draw();
+        }
+    },this);
 }
 
 MathGraph.prototype = {
-	constructor : MathGraph,
-	
+    constructor : MathGraph,
+    
     draggable : false,
     
-	Functions : {},
-	FColors : {},
-	
-	Canvas : '',
-	Ctx : '',
-	
-	Width : '',
-	Height : '',
-	
-	MaxX : 10,
-	MinX : -10,
-	
-	MaxY : 10,
-	MinY : -10,
-	
-	YTick : 1,
-	XTick : 1,
-	
-	TickLabels : true,
-	
-	// Returns the physical x-coordinate of a logical x-coordinate:
-	XC : function(x) {
-	  return (x - this.MinX) / (this.MaxX - this.MinX) * this.Width;
-	},
-	
-	// Returns the logical x-coordinate of a physical x-coordinate:
-	XCi : function(x) {
-	  return (x * (this.MaxX-this.MinX))/this.Width + this.MinX;
-	},
-	
-	// Returns the physical y-coordinate of a logical y-coordinate:
-	YC : function(y) {
-	  return this.Height - (y - this.MinY) / (this.MaxY - this.MinY) * this.Height;
-	},
-	
-	// Returns the logical y-coordinate of a physical y-coordinate:
-	YCi : function(y) {
-	  return (this.Height-y)*(this.MaxY-this.MinY)/this.Height + this.MinY;
-	},
-	
-	Draw : function() {
-		if (this.Canvas.getContext)
-		{
-			this.Ctx = this.Canvas.getContext('2d');
-			this.Ctx.clearRect(0,0,this.Width,this.Height);
+    Functions : {},
+    FColors : {},
+    
+    Canvas : '',
+    Ctx : '',
+    
+    Width : '',
+    Height : '',
+    
+    MaxX : 10,
+    MinX : -10,
+    
+    MaxY : 10,
+    MinY : -10,
+    
+    YTick : 1,
+    XTick : 1,
+    
+    TickLabels : true,
+    
+    // Returns the physical x-coordinate of a logical x-coordinate:
+    XC : function(x) {
+      return (x - this.MinX) / (this.MaxX - this.MinX) * this.Width;
+    },
+    
+    // Returns the logical x-coordinate of a physical x-coordinate:
+    XCi : function(x) {
+      return (x * (this.MaxX-this.MinX))/this.Width + this.MinX;
+    },
+    
+    // Returns the physical y-coordinate of a logical y-coordinate:
+    YC : function(y) {
+      return this.Height - (y - this.MinY) / (this.MaxY - this.MinY) * this.Height;
+    },
+    
+    // Returns the logical y-coordinate of a physical y-coordinate:
+    YCi : function(y) {
+      return (this.Height-y)*(this.MaxY-this.MinY)/this.Height + this.MinY;
+    },
+    
+    Draw : function() {
+        if (this.Canvas.getContext)
+        {
+            this.Ctx = this.Canvas.getContext('2d');
+            this.Ctx.clearRect(0,0,this.Width,this.Height);
 
-			// Draw:
-			this.DrawAxes();
-			
-			for (f in this.Functions)
-			{
-				var color = (this.FColors[f] != null) ? this.FColors[f] : '#000000';
-				this.RenderFunction(this.Functions[f], color);
-			}
-		}
-	},
-	
-	// DrawAxes draws the X ad Y axes, with tick marks.
-	DrawAxes : function() {
-		this.Ctx.font = "bold 13px sans-serif";
-		this.Ctx.strokeStyle = '#000000';
-		this.Ctx.save();
-		this.Ctx.lineWidth = 2;
-		// +Y axis
-		this.Ctx.beginPath();
-		this.Ctx.moveTo(this.XC(0),this.YC(0));
-		this.Ctx.lineTo(this.XC(0),this.YC(this.MaxY));
-		this.Ctx.stroke();
+            // Draw:
+            this.DrawAxes();
+            
+            for (f in this.Functions)
+            {
+                var color = (this.FColors[f] != null) ? this.FColors[f] : '#000000';
+                this.RenderFunction(this.Functions[f], color);
+            }
+        }
+    },
+    
+    // DrawAxes draws the X ad Y axes, with tick marks.
+    DrawAxes : function() {
+        this.Ctx.font = "bold 13px sans-serif";
+        this.Ctx.strokeStyle = '#000000';
+        this.Ctx.save();
+        this.Ctx.lineWidth = 2;
+        // +Y axis
+        this.Ctx.beginPath();
+        this.Ctx.moveTo(this.XC(0),this.YC(0));
+        this.Ctx.lineTo(this.XC(0),this.YC(this.MaxY));
+        this.Ctx.stroke();
 
-		// -Y axis
-		this.Ctx.beginPath();
-		this.Ctx.moveTo(this.XC(0),this.YC(0));
-		this.Ctx.lineTo(this.XC(0),this.YC(this.MinY));
-		this.Ctx.stroke();
+        // -Y axis
+        this.Ctx.beginPath();
+        this.Ctx.moveTo(this.XC(0),this.YC(0));
+        this.Ctx.lineTo(this.XC(0),this.YC(this.MinY));
+        this.Ctx.stroke();
 
-		// Y axis tick marks
-		var delta = this.YTick;
-		for (var i = 1; (i * delta) < this.MaxY; ++i)
-		{
-			this.Ctx.beginPath();
-			this.Ctx.moveTo(this.XC(0) - 5,this.YC(i * delta));
-			this.Ctx.lineTo(this.XC(0) + 5,this.YC(i * delta));
-			
-			if (this.TickLabels)
-			{
-				this.Ctx.textBaseline = "hanging";
-				this.Ctx.fillText(i * delta, this.XC(0)+7,this.YC(i * delta)-5);
-			}
-			
-			this.Ctx.stroke();
-		}
+        // Y axis tick marks
+        var delta = this.YTick;
+        for (var i = 1; (i * delta) < this.MaxY; ++i)
+        {
+            this.Ctx.beginPath();
+            this.Ctx.moveTo(this.XC(0) - 5,this.YC(i * delta));
+            this.Ctx.lineTo(this.XC(0) + 5,this.YC(i * delta));
+            
+            if (this.TickLabels)
+            {
+                this.Ctx.textBaseline = "hanging";
+                this.Ctx.fillText(i * delta, this.XC(0)+7,this.YC(i * delta)-5);
+            }
+            
+            this.Ctx.stroke();
+        }
 
-		var delta = this.YTick;
-		for (var i = -1; (i * delta) > this.MinY; --i)
-		{
-			this.Ctx.beginPath();
-			this.Ctx.moveTo(this.XC(0) - 5,this.YC(i * delta));
-			this.Ctx.lineTo(this.XC(0) + 5,this.YC(i * delta));
-			
-			if (this.TickLabels)
-			{
-				this.Ctx.textBaseline = "hanging";
-				this.Ctx.fillText(i * delta, this.XC(0)+7,this.YC(i * delta)-5);
-			}
-			
-			this.Ctx.stroke();
-		}
+        var delta = this.YTick;
+        for (var i = -1; (i * delta) > this.MinY; --i)
+        {
+            this.Ctx.beginPath();
+            this.Ctx.moveTo(this.XC(0) - 5,this.YC(i * delta));
+            this.Ctx.lineTo(this.XC(0) + 5,this.YC(i * delta));
+            
+            if (this.TickLabels)
+            {
+                this.Ctx.textBaseline = "hanging";
+                this.Ctx.fillText(i * delta, this.XC(0)+7,this.YC(i * delta)-5);
+            }
+            
+            this.Ctx.stroke();
+        }
 
-		// +X axis
-		this.Ctx.beginPath();
-		this.Ctx.moveTo(this.XC(0),this.YC(0));
-		this.Ctx.lineTo(this.XC(this.MaxX),this.YC(0));
-		this.Ctx.stroke();
+        // +X axis
+        this.Ctx.beginPath();
+        this.Ctx.moveTo(this.XC(0),this.YC(0));
+        this.Ctx.lineTo(this.XC(this.MaxX),this.YC(0));
+        this.Ctx.stroke();
 
-		// -X axis
-		this.Ctx.beginPath();
-		this.Ctx.moveTo(this.XC(0),this.YC(0));
-		this.Ctx.lineTo(this.XC(this.MinX),this.YC(0));
-		this.Ctx.stroke();
+        // -X axis
+        this.Ctx.beginPath();
+        this.Ctx.moveTo(this.XC(0),this.YC(0));
+        this.Ctx.lineTo(this.XC(this.MinX),this.YC(0));
+        this.Ctx.stroke();
 
-		// X tick marks
-		var delta = this.XTick;
-		for (var i = 1; (i * delta) < this.MaxX; ++i)
-		{
-			this.Ctx.beginPath();
-			this.Ctx.moveTo(this.XC(i * delta),this.YC(0)-5);
-			this.Ctx.lineTo(this.XC(i * delta),this.YC(0)+5);
-			
-			if (this.TickLabels)
-			{
-				this.Ctx.textBaseline = "hanging";
-				this.Ctx.fillText(i * delta, this.XC(i * delta)-3,this.YC(0)+7);
-			}
-			
-			this.Ctx.stroke();
-		}
+        // X tick marks
+        var delta = this.XTick;
+        for (var i = 1; (i * delta) < this.MaxX; ++i)
+        {
+            this.Ctx.beginPath();
+            this.Ctx.moveTo(this.XC(i * delta),this.YC(0)-5);
+            this.Ctx.lineTo(this.XC(i * delta),this.YC(0)+5);
+            
+            if (this.TickLabels)
+            {
+                this.Ctx.textBaseline = "hanging";
+                this.Ctx.fillText(i * delta, this.XC(i * delta)-3,this.YC(0)+7);
+            }
+            
+            this.Ctx.stroke();
+        }
 
-		var delta = this.XTick;
-		for (var i = -1; (i * delta) > this.MinX; --i)
-		{
-			this.Ctx.beginPath();
-			this.Ctx.moveTo(this.XC(i * delta),this.YC(0)-5);
-			this.Ctx.lineTo(this.XC(i * delta),this.YC(0)+5);
-			
-			if (this.TickLabels)
-			{
-				this.Ctx.textBaseline = "hanging";
-				this.Ctx.fillText(i * delta, this.XC(i * delta)-7,this.YC(0)+7);
-			}
-			
-			this.Ctx.stroke();
-		}
-		
-		
-		this.Ctx.textBaseline = "top";
-		this.Ctx.fillText("MathGraph", 8, 5);
-		
-		this.Ctx.restore();
-	},
-	
-	// Draws a dot and coordinates when click near a function
-	DrawDot : function(f,x,clean)
-	{
-		y = this.Functions[f](x);
-		yr = Math.round(y);
-		xr = Math.round(x);
-		
-		if (clean)
-		{
-			this.Draw();
-		}
-		
-		this.Ctx.beginPath();
-		this.Ctx.arc(this.XC(x), this.YC(y), 4, 0, Math.PI*2, true);
-		this.Ctx.closePath();
-		this.Ctx.fill();
-		
-		this.Ctx.textBaseline = "hanging";
-		this.Ctx.fillText('(' + xr + ',' + yr + ')', this.XC(x)+10,this.YC(y));
-		
-		if (clean)
-		{
-			this.Ctx.textBaseline = "top";
-			this.Ctx.fillText('x = ' + Math.round(x*Math.pow(10,3))/Math.pow(10,3), 8, 30);
-			this.Ctx.fillText('y = ' + Math.round(y*Math.pow(10,3))/Math.pow(10,3), 8, 45);
-			
-			dydx = (this.Functions[f](x+0.000001) - this.Functions[f](x))/0.000001;
-			this.Ctx.fillText('dy/dx \u2248 ' + Math.round(dydx*Math.pow(10,2))/Math.pow(10,2), 8, 65);
-		}
-		
-	},
-	
-	// RenderFunction(f) renders the input funtion f on the canvas.
-	RenderFunction : function(f,color) {
-		// When rendering, XSTEP determines the horizontal distance between points:
-		var XSTEP = (this.MaxX-this.MinX)/this.Width ;
+        var delta = this.XTick;
+        for (var i = -1; (i * delta) > this.MinX; --i)
+        {
+            this.Ctx.beginPath();
+            this.Ctx.moveTo(this.XC(i * delta),this.YC(0)-5);
+            this.Ctx.lineTo(this.XC(i * delta),this.YC(0)+5);
+            
+            if (this.TickLabels)
+            {
+                this.Ctx.textBaseline = "hanging";
+                this.Ctx.fillText(i * delta, this.XC(i * delta)-7,this.YC(0)+7);
+            }
+            
+            this.Ctx.stroke();
+        }
+        
+        
+        this.Ctx.textBaseline = "top";
+        this.Ctx.fillText("MathGraph", 8, 5);
+        
+        this.Ctx.restore();
+    },
+    
+    // Draws a dot and coordinates when click near a function
+    DrawDot : function(f,x,clean)
+    {
+        y = this.Functions[f](x);
+        yr = Math.round(y);
+        xr = Math.round(x);
+        
+        if (clean)
+        {
+            this.Draw();
+        }
+        
+        this.Ctx.beginPath();
+        this.Ctx.arc(this.XC(x), this.YC(y), 4, 0, Math.PI*2, true);
+        this.Ctx.closePath();
+        this.Ctx.fill();
+        
+        this.Ctx.textBaseline = "hanging";
+        this.Ctx.fillText('(' + xr + ',' + yr + ')', this.XC(x)+10,this.YC(y));
+        
+        if (clean)
+        {
+            this.Ctx.textBaseline = "top";
+            this.Ctx.fillText('x = ' + Math.round(x*Math.pow(10,3))/Math.pow(10,3), 8, 30);
+            this.Ctx.fillText('y = ' + Math.round(y*Math.pow(10,3))/Math.pow(10,3), 8, 45);
+            
+            dydx = (this.Functions[f](x+0.000001) - this.Functions[f](x))/0.000001;
+            this.Ctx.fillText('dy/dx \u2248 ' + Math.round(dydx*Math.pow(10,2))/Math.pow(10,2), 8, 65);
+        }
+        
+    },
+    
+    // RenderFunction(f) renders the input funtion f on the canvas.
+    RenderFunction : function(f,color) {
+        // When rendering, XSTEP determines the horizontal distance between points:
+        var XSTEP = (this.MaxX-this.MinX)/this.Width ;
 
-		var first = true;
+        var first = true;
 
-		this.Ctx.beginPath();
-		for (var x = this.MinX; x <= this.MaxX; x += XSTEP)
-		{
-			var y = f(x);
-			if (first)
-			{
-				this.Ctx.moveTo(this.XC(x),this.YC(y));
-				first = false;
-			}
-			else
-			{
-				this.Ctx.lineTo(this.XC(x),this.YC(y));
-			}
-		}
-		this.Ctx.strokeStyle = color;
-		this.Ctx.stroke();
-	},
-	
-	/* code from http://helephant.com/2008/04/26/objects-event-handlers-and-this-in-javascript/ */
-	_addEvent : function(element, eventName, eventHandler, scope){
-		var scopedEventHandler = scope ? function(e) { eventHandler.apply(scope, [e]); } : eventHandler;
-		if(document.addEventListener)
-			element.addEventListener(eventName, scopedEventHandler, false);
-		else if(document.attachEvent)
-			element.attachEvent("on"+eventName, scopedEventHandler);
-	}
+        this.Ctx.beginPath();
+        for (var x = this.MinX; x <= this.MaxX; x += XSTEP)
+        {
+            var y = f(x);
+            if (first)
+            {
+                this.Ctx.moveTo(this.XC(x),this.YC(y));
+                first = false;
+            }
+            else
+            {
+                this.Ctx.lineTo(this.XC(x),this.YC(y));
+            }
+        }
+        this.Ctx.strokeStyle = color;
+        this.Ctx.stroke();
+    },
+    
+    /* code from http://helephant.com/2008/04/26/objects-event-handlers-and-this-in-javascript/ */
+    _addEvent : function(element, eventName, eventHandler, scope){
+        var scopedEventHandler = scope ? function(e) { eventHandler.apply(scope, [e]); } : eventHandler;
+        if(document.addEventListener)
+            element.addEventListener(eventName, scopedEventHandler, false);
+        else if(document.attachEvent)
+            element.attachEvent("on"+eventName, scopedEventHandler);
+    }
 };
 
 
